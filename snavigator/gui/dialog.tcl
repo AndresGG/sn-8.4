100a1d3037739708f2a59ede6d174be491c24ad3
Andres Garcia <fandom@telefonica.net>
2014-08-19 12:24:40 +0200

    (INTERNAL) Removed sn_motif_buttons

--- snavigator/gui/dialog.tcl
+++ snavigator/gui/dialog.tcl
@@ -79,68 +79,6 @@ proc sn_ttk_buttons {frm pos default args} {
     }
 }
 
-#
-# This procedure creates buttons in a frame.
-#
-# Arguments:
-# w -		Frame for buttons.
-# default -	Index of button that is to display the default ring
-#		(-1 means none).
-# pos -		top, bottom ...
-# args -	One or more strings to display in buttons across the
-#		bottom of the dialog box.
-proc sn_motif_buttons {frm pos default args} {
-    global tcl_platform
-    if {${frm} == "."} {
-        set w ""
-    } else {
-        set w ${frm}
-    }
-
-    if {$tcl_platform(platform) == "windows"} {
-        frame ${w}.button -relief raised -bd 0
-        pack ${w}.button -side ${pos} -fill both -padx 5 -pady 5
-    } else {
-        frame ${w}.button -relief raised -bd 1
-        pack ${w}.button -side ${pos} -fill both
-    }
-
-    #expand the width of the buttons
-    set len 10
-    foreach but ${args} {
-        set ll [string length ${but}]
-        if {${ll} < 14 && ${len} < ${ll}} {
-            set len ${ll}
-        }
-    }
-
-    set i 0
-    foreach but ${args} {
-        set cmd [list button ${w}.button_${i}]
-        if {[string index ${but} 0] == "@"} {
-            lappend cmd -image [string range ${but} 1 end]
-        } else {
-            lappend cmd -text ${but}
-            set len [string length ${but}]
-            if {${len} <= 6 && ${len} != 0} {
-                lappend cmd -width 7
-            }
-        }
-        if {${i} == ${default}} {
-            lappend cmd -default active
-        }
-        if {[string length ${but}] < ${len}} {
-            lappend cmd -width ${len}
-        }
-
-        eval ${cmd}
-        pack ${w}.button_${i} -in ${w}.button -side left -expand 1 -padx 3\
-          -pady 2 -ipadx 1m
-
-        incr i
-    }
-}
-
 itcl::class sourcenav::Dialog {
     inherit sourcenav::Window
 
